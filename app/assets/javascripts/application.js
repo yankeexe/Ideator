06d59e63af38cdc0a0f863707a01bf5f2bfcd924
Yankee Maharjan <yankee.exe@gmail.com>
2016-11-02 16:04:05 +0000

    Submit Done

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,7 +12,6 @@
 //
 //= require jquery
 //= require bootstrap-sprockets
-//= require jquery
 //= require jquery_ujs
 //= require turbolinks
 //= require_tree .
--- app/controllers/ideas_controller.rb
+++ app/controllers/ideas_controller.rb
@@ -2,4 +2,11 @@ class IdeasController < ApplicationController
     def index 
         @ideas=Idea.all
     end
+    def create
+        @ideas= Idea.create(idea_params)
+        redirect_to root_path
+    end
+    def idea_params
+        params.require(:idea).permit(:description, :author)
+    end
 end
