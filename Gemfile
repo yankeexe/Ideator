3be1b9e39c8a14a534d8c1c38d3802a85771e432
Yankee Maharjan <yankee.exe@gmail.com>
2016-11-06 07:17:22 +0000

    Last tweaks

--- Gemfile
+++ Gemfile
@@ -43,6 +43,7 @@ group :development do
   gem 'web-console', '~> 2.0'
   
   gem 'simple_form'
+  gem 'will_paginate', '~> 3.1.0'
 
   # Spring speeds up development by keeping your application running in the background. Read more: https://github.com/rails/spring
   gem 'spring'
--- app/controllers/ideas_controller.rb
+++ app/controllers/ideas_controller.rb
@@ -1,33 +1,41 @@
 class IdeasController < ApplicationController
-    def index 
-        @ideas=Idea.all
+    def index
+        @ideas=Idea.order("created_at DESC").paginate(:page => params[:page],:per_page => 5)
     end
     def create
-        @ideas= Idea.create(idea_params)
+        @idea= Idea.create(idea_params)
         if @idea.valid?
-            #implement later
-        else 
-            #implement later
+            flash[:success]= "Your idea has been posted"
+        else
+            flash[:alert] = "There is an error"
         end
         redirect_to root_path
     end
+    
     def edit
-        @idea = Idea.find(params[:id])
-        if @idea.update(idea_params)
+        @idea= Idea.find(params[:id])
+    end
+    
+    def update
+         @idea = Idea.find(params[:id])
+         if @idea.update(idea_params)
+            flash[:success] = "Your Idea has been updated."
             redirect_to root_path
-        else
+         else
+            flash[:alert]= "An error occured while updating the Record"
             redirect_to edit_idea_path(params[:id])
-        end
+         end
     end
     
     def destroy
-    @idea = Idea.find(params[:id])
-   @idea.destroy
-   redirect_to root_path
-        end
+        @idea =Idea.find(params[:id])
+        @idea.destroy
+        flash[:success]="The idea has been successfully deleted"
+        redirect_to root_path
+    end
     
-    
+    private
     def idea_params
-        params.require(:idea).permit(:description, :author)
+        params.require(:idea).permit(:description,:author)
     end
 end
